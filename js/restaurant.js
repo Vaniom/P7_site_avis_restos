36a11a16e2c3c4afd525920d7efd9d4b3ba2c4d8
Vaniom <florent.p83@gmail.com>
2019-01-09 10:57:14 +0100

    ajout fonctionalité : évaluer un resto

--- js/restaurant.js
+++ js/restaurant.js
@@ -1,5 +1,5 @@
 function Restaurant(name, adress, lat, lng) {
-    var that = this;
+    var self = this;
     this.name = name;
     this.address = adress;
     this.pos = {
@@ -8,67 +8,70 @@ function Restaurant(name, adress, lat, lng) {
     };
     this.ratings = "";
     this.marker = new google.maps.Marker({
-        position: that.pos,
+        position: self.pos,
         map: map,
-        title: that.name,
+        title: self.name,
         icon: './img/restaurant.png',
         animation: null // Attention: à definir au moment de l'instanciation pour prise en compte lors de l'appel de la fonction toggleBounce()
     });
     this.calculateAverage = function() {
         var somme = 0;
-        for (var i = 0; i < that.ratings.length; i++) {
-            somme = that.ratings[i].stars + parseInt(somme);
+        for (var i = 0; i < self.ratings.length; i++) {
+            somme = self.ratings[i].stars + parseInt(somme);
         }
-        var moy = somme / (that.ratings.length);
+        var moy = somme / (self.ratings.length);
         return Number(moy.toFixed(1));
     };
     this.li = document.createElement("li");
     this.title = document.createElement("h3");
     this.note = document.createElement("p");
+    this.voteBtn = document.createElement('div');
     this.showInfos = function () {//Mise en forme des informations et gestion du clic sur le titre
         var listUL = document.getElementById("listUL");
-        that.title.textContent = that.name;
-        var grade = that.calculateAverage();
-        that.colorTheStars(grade, that.note);
-        that.li.appendChild(that.title);
-        that.li.id = that.name;
-        that.li.classList.add("clickable");
-        var voteBtn = document.createElement('div');
-        voteBtn.innerHTML = "<button type='button' id='avisBtn' class='btn btn-outline-primary btn-sm voteBtn' data-toggle='modal' data-target='#exampleModalCenter'>Je donne mon avis</button>";
-        voteBtn.addEventListener("click", function(){
-            document.getElementById("nameHere").textContent = that.name;
+        self.title.textContent = self.name;
+        var grade = self.calculateAverage();
+        self.colorTheStars(grade, self.note);
+        self.li.appendChild(self.title);
+        self.li.id = self.name;
+        self.li.classList.add("clickable");
+        //var voteBtn = document.createElement('div');
+        self.voteBtn.id = 'voteBtn';
+        self.voteBtn.innerHTML = "<button type='button' id='avisBtn' class='btn btn-outline-primary btn-sm voteBtn'>Je donne mon avis</button>";
+        self.li.appendChild(self.voteBtn);
+        var listener1 = self.voteBtn.addEventListener("click", function(){
+            self.voteBtn.classList.add('hide');
+            self.constructModal();
+            self.vote();
         });
-        
-        that.li.appendChild(voteBtn);
-        that.li.appendChild(that.note);
-        listUL.appendChild(that.li);
+        self.li.appendChild(self.note);
+        listUL.appendChild(self.li);
         if (this.isInRectangle() == true) {
-            that.li.classList.add("show");
+            self.li.classList.add("show");
         }else {
-            that.li.classList.remove("show");
+            self.li.classList.remove("show");
         }
         var contentDiv = document.createElement('div');
         var addressSection = document.createElement('p');
         var imgSection = document.createElement('p');
         imgSection.classList.add("streetviewSection");
-        imgSection.innerHTML = '<img src="' + that.streetviewImage + '" class="streetviewImage" alt="image streetview" />';
-        addressSection.textContent = that.address;
+        imgSection.innerHTML = '<img src="' + self.streetviewImage + '" class="streetviewImage" alt="image streetview" />';
+        addressSection.textContent = self.address;
         addressSection.classList.add("address");
         contentDiv.appendChild(addressSection);
         contentDiv.appendChild(imgSection);
-        for (var i = 0; i < that.ratings.length; i++) {
+        for (var i = 0; i < self.ratings.length; i++) {
             var insertComment = document.createElement("div");
-            var stars = that.ratings[i].stars;
-            var avis = that.ratings[i].comment;
+            var stars = self.ratings[i].stars;
+            var avis = self.ratings[i].comment;
             insertComment.innerHTML = "<p class='comment'>Commentaire N° " + (i+1) + ":  " + avis + "<br/>Note : " + stars + "</p>";
             contentDiv.appendChild(insertComment);
         }
-        that.li.appendChild(contentDiv);
+        self.li.appendChild(contentDiv);
         contentDiv.classList.add("collapse");
         var restoArray = myLayout.restoArray;
-        that.title.addEventListener("click", function(){// Ecouteur d'evenement au clic
+        self.title.addEventListener("click", function(){// Ecouteur d'evenement au clic
         for (var i = 0; i < restoArray.length; i++) {
-            if (restoArray[i].name != that.name) {
+            if (restoArray[i].name != self.name) {
                 restoArray[i].li.classList.remove("clicked");
                 restoArray[i].marker.setAnimation(null);                
                 var children = restoArray[i].li.childNodes;
@@ -77,10 +80,9 @@ function Restaurant(name, adress, lat, lng) {
                 });    
             } else {}
         }
-            that.toggleBounce();// On declenche l'animation du marqueur
-            that.li.classList.toggle("clicked");// On permutte la classe sur l'entrée de liste correspondante
-            //that.detailed = true;
-            var childNodes = that.li.childNodes;
+            self.toggleBounce();// On declenche l'animation du marqueur
+            self.li.classList.toggle("clicked");// On permutte la classe sur l'entrée de liste correspondante
+            var childNodes = self.li.childNodes;
             // On affiche les elements attachés
             Array.prototype.filter.call(childNodes, function(element){
                 element.classList.toggle("showElt");
@@ -88,21 +90,21 @@ function Restaurant(name, adress, lat, lng) {
         });
     };
     this.toggleBounce = function () { // gestion de l'animation du marqueur
-        if (that.marker.getAnimation() !== null) {
-            that.marker.setAnimation(null);
-          } else if (that.marker.getAnimation() == null) {
-            that.marker.setAnimation(google.maps.Animation.BOUNCE);
+        if (self.marker.getAnimation() !== null) {
+            self.marker.setAnimation(null);
+          } else if (self.marker.getAnimation() == null) {
+            self.marker.setAnimation(google.maps.Animation.BOUNCE);
           }
     };
     this.isInRectangle = function () {
-        var pointLat = that.pos.lat;
-        var pointLng = that.pos.lng;
+        var pointLat = self.pos.lat;
+        var pointLng = self.pos.lng;
         var point = new google.maps.LatLng(pointLat, pointLng);
         if (rectangle.getBounds().contains(point)){
-            console.log(that.name + " IN");
+            console.log(self.name + " IN");
             return true;
         }else {
-            console.log(that.name + " OUT");
+            console.log(self.name + " OUT");
             return false;
         }
     };
@@ -147,8 +149,8 @@ function Restaurant(name, adress, lat, lng) {
             averageStars.appendChild(noStars);
         }
         var nbAvis = document.createElement("span");
-        if (that.ratings.length > 0){
-            nbAvis.textContent = " " + that.ratings.length + " avis.";
+        if (self.ratings.length > 0){
+            nbAvis.textContent = " " + self.ratings.length + " avis.";
         }else {
             nbAvis.textContent = "Aucun avis publié.";
         }
@@ -159,22 +161,106 @@ function Restaurant(name, adress, lat, lng) {
     this.clicOnMarker = function () {
         var infoContent = document.createElement("div");
         var infoMoyenne = document.createElement("p");
-        var grade = that.calculateAverage();
+        var grade = self.calculateAverage();
         var infoTitle = document.createElement("h3");
         var infoStreetview = document.createElement("p");
-        infoTitle.textContent = that.name;
+        infoTitle.textContent = self.name;
         infoContent.appendChild(infoTitle);
-        infoStreetview.innerHTML = '<img src="' + that.streetviewImage + '" class="streetviewImage" alt="image streetview" />';         
-        infoMoyenne.textContent = that.ratings.length + " avis " + that.colorTheStars(grade, infoContent);
+        infoStreetview.innerHTML = '<img src="' + self.streetviewImage + '" class="streetviewImage" alt="image streetview" />';         
+        infoMoyenne.textContent = self.ratings.length + " avis " + self.colorTheStars(grade, infoContent);
         infoContent.appendChild(infoStreetview);
-        that.marker.addListener("click", function(){
+        self.marker.addListener("click", function(){
             var infoWindow = new google.maps.InfoWindow({
                 maxWidth: 300,
-                position: that.pos,
+                position: self.pos,
                 map: map,
                 content: infoContent
             }); 
         });
     };
+    this.userComment = "";
+    this.constructModal = function(listener){
+        var formDiv = document.createElement('div');
+        formDiv.innerHTML = '<div id="voteForm"><form class="modal-form"><p>Evaluez :<br /><div class="rating"><i id="note1" class="vote far fa-star"></i><i id="note2" class="vote far fa-star"></i><i id="note3" class="vote far fa-star"></i><i id="note4" class="vote far fa-star"></i><i id="note5" class="vote far fa-star"></i></div></p><label for="commentaire">Commentez :</label><textarea class="form-control" id="commentaire" rows="5" required></textarea></form><button type="submit" id="saveBtn" class="btn btn-primary">Envoyer</button><button type="button" id="closeBtn" class="btn btn-secondary">Fermer</button></div>';
+        $(self.li).append(formDiv);
+        var saveBtn = document.getElementById('saveBtn');
+        var closeBtn = document.getElementById('closeBtn');
+        saveBtn.addEventListener('click', function(){
+            self.userComment = document.getElementById('commentaire').value;
+            console.log('commentaire = ' + self.userComment + ' / note = ' + self.userNote);
+            var rating = {stars: self.userNote, comment: self.userComment};
+            self.ratings.push(rating);
+            self.voteBtn.classList.remove("hide");
+            formDiv.innerHTML = "";
+            self.userComment = "";
+            self.userNote = 0;
+            myLayout.listUpdate();
+        });
+        closeBtn.addEventListener('click', function(){
+            self.voteBtn.classList.remove("hide");
+            formDiv.innerHTML = "";
+            self.userComment = "";
+            self.userNote = 0;
+        });
+    };
     this.filter = "show";
+    this.userNote = 0;
+    this.vote = function(){
+        self.userNote = 0;
+        var userComment;
+        var textarea = document.getElementById('commentaire');
+        var voteForm = document.getElementById('voteForm');
+        textarea.value = "";
+        var star1 = document.getElementById('note1');
+        star1.addEventListener('click', function(){
+            star1.classList.toggle('fas');
+            star1.classList.toggle('far');
+            self.userNote = voteForm.getElementsByClassName('fas').length;
+        });
+        var star2 = document.getElementById('note2');
+        star2.addEventListener('click', function(){
+            star1.classList.add('fas');
+            star1.classList.remove('far');
+            star2.classList.toggle('fas');
+            star2.classList.toggle('far');
+            self.userNote = voteForm.getElementsByClassName('fas').length;
+        });
+        var star3 = document.getElementById('note3');
+        star3.addEventListener('click', function(){
+            star1.classList.add('fas');
+            star1.classList.remove('far');
+            star2.classList.add('fas');
+            star2.classList.remove('far');
+            star3.classList.toggle('fas');
+            star3.classList.toggle('far');
+            self.userNote = voteForm.getElementsByClassName('fas').length;
+        });
+        var star4 = document.getElementById('note4');
+        star4.addEventListener('click', function(){
+            star1.classList.add('fas');
+            star1.classList.remove('far');
+            star2.classList.add('fas');
+            star2.classList.remove('far');
+            star3.classList.add('fas');
+            star3.classList.remove('far');
+            star4.classList.toggle('fas');
+            star4.classList.toggle('far');
+            self.userNote = voteForm.getElementsByClassName('fas').length;
+        });
+        var star5 = document.getElementById('note5');
+        star5.addEventListener('click', function(){
+            star1.classList.add('fas');
+            star1.classList.remove('far');
+            star2.classList.add('fas');
+            star2.classList.remove('far');
+            star3.classList.add('fas');
+            star3.classList.remove('far');
+            star4.classList.add('fas');
+            star4.classList.remove('far');
+            star5.classList.toggle('fas');
+            star5.classList.toggle('far');
+            self.userNote = voteForm.getElementsByClassName('fas').length;
+        });
+    };
 }
+
